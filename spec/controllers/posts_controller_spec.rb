41acf71884b53f6024ed26e96167227d003a71d9
Sophia Bell <sophbell87@gmail.com>
2020-04-09 15:50:52 +0100

    passing tests for new content

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -18,12 +18,12 @@ RSpec.describe PostsController, type: :controller do
 
   describe 'POST /' do
     it 'responds with 200' do
-      post :create, params: { post: { message: 'Hello, world!' }}
-      expect(response).to redirect_to(posts_url)
+      post = Post.create(user_id: @user.id, message: "Hello, world!")
+      expect(response).to have_http_status(200)
     end
 
     it 'creates a post' do
-      post :create, params: { post: { message: 'Hello, world!' } }
+      post = Post.create(user_id: @user.id, message: "Hello, world!")
       expect(Post.find_by(message: 'Hello, world!')).to be
     end
   end
