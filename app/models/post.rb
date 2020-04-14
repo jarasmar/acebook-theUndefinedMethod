905a33cd9f9d2f9e439db2aac40b63455ad27b0c
Sophia Bell <sophbell87@gmail.com>
2020-04-14 15:47:08 +0100

    adding in descending order scope to posts and comments, adding account and public html layouts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  default_scope { order created_at: :desc}
   validates_presence_of :message
   belongs_to :user
+  has_many :comments
 
   def as_json(options = {})
     super(options.merge(include: [:user, comments: {include: :user}]))
