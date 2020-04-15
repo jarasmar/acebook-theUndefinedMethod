ad77ea36d8d5ef87899b0b7c88d12ad6178c0e5a
nimzyow <n_soufiani@hotmail.com>
2020-04-15 15:33:17 +0100

    fixing multiple like bug

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,16 +1,17 @@
 $(document).ready(function () {
-  $("#heart").click(function () {
-    let innerClass = $("#heart").attr("class");
+  $(`#likePost`).click(function () {
+    console.log("hello world!");
+    let innerClass = $(".fa-heart").attr("class");
     if (innerClass === "fa fa-heart unlike fa-2x") {
-      $("#heart").removeClass().addClass("fa fa-heart like fa-2x");
-      let count = $("#like-count").html().split(" ")[0];
+      $(".fa-heart").removeClass().addClass("fa fa-heart like fa-2x");
+      let count = $(".like-count").html().split(" ")[0];
       let countInt = parseInt(count);
-      $("#like-count").html(`${countInt + 1} Likes`);
+      $(".like-count").html(`${countInt + 1} Likes`);
     } else {
-      $("#heart").removeClass().addClass("fa fa-heart unlike fa-2x");
-      let count = $("#like-count").html().split(" ")[0];
+      $(".fa-heart").removeClass().addClass("fa fa-heart unlike fa-2x");
+      let count = $(".like-count").html().split(" ")[0];
       let countInt = parseInt(count);
-      $("#like-count").html(`${countInt - 1} Likes`);
+      $(".like-count").html(`${countInt - 1} Likes`);
     }
   });
 });
