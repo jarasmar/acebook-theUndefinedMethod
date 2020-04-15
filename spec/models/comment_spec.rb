210165d10a80e7204f06bd6503c3b783636279ff
Sophia Bell <sophbell87@gmail.com>
2020-04-15 15:57:22 +0100

    model and controller tests

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+
+  it 'cannot be edited after ten minutes' do
+    comment = Comment.create(created_at: Time.now)
+    Timecop.travel(Time.now + 10.minutes, 1.second) do
+      expect(comment.comment_time_limit).to eq false
+    end
+  end
+
+  it 'can be edited before ten minutes' do
+    comment = Comment.create(created_at: Time.now)
+    Timecop.travel(Time.now + 9.minutes, 59.seconds) do
+      expect(comment.comment_time_limit).to eq true
+    end
+  end
+
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,5 +7,20 @@ RSpec.describe Post, type: :model do
   it 'is not valid without a message' do
     expect(subject).to_not be_valid
   end
+
+  it 'cannot be edited after ten minutes' do
+    post = Post.create(created_at: Time.now)
+    Timecop.travel(Time.now + 10.minutes, 1.second) do
+      expect(post.post_time_limit).to eq false
+    end
+  end
+
+  it 'can be edited before ten minutes' do
+    post = Post.create(created_at: Time.now)
+    Timecop.travel(Time.now + 9.minutes, 59.seconds) do
+      expect(post.post_time_limit).to eq true
+    end
+  end
+  
   
 end
