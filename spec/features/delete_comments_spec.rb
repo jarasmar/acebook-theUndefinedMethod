5779fd251e2575c6c7a482132f3cc26febd5e12a
Sophia Bell <sophbell87@gmail.com>
2020-04-16 13:03:59 +0100

    editing comments tests

---
+++ spec/features/delete_comments_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Delete comments', type: :feature do
+
+  before(:each) do
+    sign_up
+    submit_post
+  end
+
+  scenario "Can delete your own comments" do
+    first_comment
+    click_link "Delete comment"
+
+    expect(page).not_to have_content('First comment')
+  end
+
+  scenario "Cannot delete someone else's comment" do
+    first_comment
+    click_link 'Logout'
+    sign_up_second_user
+    visit '/posts'
+    click_button "delete comment"
+
+    expect(page).to have_content('First comment')
+  end
+
+end
--- spec/features/submit_comments_spec.rb
+++ spec/features/submit_comments_spec.rb
@@ -29,23 +29,6 @@ RSpec.feature 'Comment on posts', type: :feature do
     expect("Second comment").to appear_before("First comment")
   end
 
-  scenario "Can delete your own comments" do
-    first_comment
-    click_link "Delete comment"
-
-    expect(page).not_to have_content('First comment')
-  end
-
-  scenario "Cannot delete someone else's comment" do
-    first_comment
-    click_link 'Logout'
-    sign_up_second_user
-    visit '/posts'
-    click_button "delete comment"
-
-    expect(page).to have_content('First comment')
-  end
-
   scenario "Can see how long ago someone posted their comment" do
     first_comment
 
